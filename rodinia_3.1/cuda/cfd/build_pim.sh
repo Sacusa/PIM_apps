fbf013211433a7542ca62adeb76a3ea0deb1565e
Sacusa <sudhanshugupta96@gmail.com>
2023-04-13 12:17:47 -0400

    Add CFD

---
+++ rodinia_3.1/cuda/cfd/build_pim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <kernel name> <num rows>"
+    exit -1
+fi
+
+pim_kernel=$1
+num_rows=$2
+
+declare -a kernels=("euler3d" "euler3d_double" "pre_euler3d" "pre_euler3d_double")
+
+for kernel in "${kernels[@]}"; do
+    cp ${kernel}_cu.template ${kernel}.cu
+    sed -i 's/PIM_KERNEL_INSTANTIATION/pim_state_t *pim_state = init_pim('"${pim_kernel}"', 1048576, '"${num_rows}"');/g' ${kernel}.cu
+done
+
+make
+mkdir -p bin
+
+for kernel in "${kernels[@]}"; do
+    #rm ${kernel}.cu
+    mv ${kernel} bin/${kernel}_${pim_kernel}_${num_rows}
+done
